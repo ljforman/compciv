45d75a613ad9663dd717f43dd0e5ba8843935094
Laura Jessica Forman <lforman@corn36.stanford.edu>
2015-01-20 20:29:41 -0800

    helper.sh was made

---
+++ homework/ssa-baby-name-fun/helper.sh
@@ -0,0 +1,10 @@
+mkdir -p data-hold
+cd data-hold
+curl -o namesbystate.zip http://stash.compciv.org/ssa_baby_names/namesbystate.zip
+unzip namesbystate.zip
+cd ..
+
+cd data-hold
+curl -o names.zip http://stash.compciv.org/ssa_baby_names/names.zip
+unzip names.zip
+cd ..
